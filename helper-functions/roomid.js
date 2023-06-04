1ea8f34ff654a075f4f94441df74590bbe1b34ce
Jake Shin <jakeshin2004@gmail.com>
2023-06-04 01:32:15 -0400

    generate unique room id

---
+++ helper-functions/roomid.js
@@ -0,0 +1,30 @@
+function genRandomID() {
+    let a = Math.floor(Math.random() * 26) + "A".charCodeAt(0);
+    let b = Math.floor(Math.random() * 26) + "A".charCodeAt(0);
+    let c = Math.floor(Math.random() * 26) + "A".charCodeAt(0);
+    let d = Math.floor(Math.random() * 26) + "A".charCodeAt(0);
+    return String.fromCharCode(a, b, c, d);
+}
+
+function codeExists(id, curRooms) {
+    var idExists = false;
+    for (let room of curRooms) {
+        if (id === room) {
+            idExists = true;
+            break;
+        }
+    }
+    return idExists;
+}
+
+function genRoomID(curRooms) {
+    var id = "";
+    var idExists = true;
+    while (idExists) {
+        id = genRandomID();
+        idExists = codeExists(id, curRooms);
+    }
+    return id;
+}
+
+module.exports = { genRoomID };
